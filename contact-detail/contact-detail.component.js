be64756720d8979cf6e9468c8d22b00f5072ee26
gbonnycastle <gbonnycastle@gmail.com>
2016-07-25 16:35:08 -0700

    Refactor Contact Details Template and Controller

--- contact-detail/contact-detail.component.js
+++ contact-detail/contact-detail.component.js
@@ -1,5 +1,6 @@
 'use strict';
 
+		
 // Register `contactDetail` component, along with its associated controller and template
 angular.
   module('contactDetail').
@@ -8,7 +9,43 @@ angular.
     controller: ['$routeParams', 'Contact',
       function ContactDetailController($routeParams, Contact) {
         var self = this;
-		self.contact = Contact.get({id: $routeParams.contactId});
-	  }
-	]
-  });
+		
+		// GET Contact data from server
+		self.contact = Contact.get({id: $routeParams.contactId}, function(contact){
+			self.setModel(contact.contact);
+			self.setFormData(contact.contact);
+		});
+		
+		self.setModel = function setModel(contact){
+			self.m = contact;
+		}
+		
+		self.setFormData = function setFormData(contact){
+			self.f = contact;
+		} 
+		
+		self.toggle = function toggle(state){
+			state = !state;
+		}
+
+		self.locDescOptions = [
+		  { 'id':1, 'name':'Main' }, 
+		  { 'id':2, 'name':'Office' },
+		  { 'id':3, 'name':'Home' }
+		];
+		
+//		self.display = element(b.css('div.display'));
+		
+/*		it('should check ng-show / ng-hide', function(){
+			expect(edit.isDisplayed()).toBeFalsy();
+			expect(display.isDisplayed()).toBeTruthy();
+			
+			element(by.model('checked')).click();
+			
+			expect(edit.isDisplayed()).toBeTruthy();
+			expect(display.isDisplayed()).toBeFalsy();
+			
+		}); */
+	  } // function ContactDetailController
+	] // controller
+  }); // component
